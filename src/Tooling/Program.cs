2160dc33b7522e339c422c8190619439476b458d
ondrej.pajgrt <pajgrt.ondrej@gmail.com>
2019-04-08 08:25:42 +0200

    01-Tooling

---
+++ src/Tooling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Tooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var version = typeof(Program).Assembly.GetName().Version;
+            //Console.WriteLine(version);
+        }
+    }
+}
