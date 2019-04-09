aafe7c884367df7ba62a20722bba82b71560824b
ondrej.pajgrt <pajgrt.ondrej@gmail.com>
2019-04-09 07:04:59 +0200

    Settings update

---
+++ src/Settings/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Settings.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+        }
+
+
+        [HttpGet]
+        public ActionResult<string> GetValues()
+        {
+            return Ok("ok");
+        }
+    }
+}
--- src/Settings/SettingsClass.cs
+++ src/Settings/SettingsClass.cs
@@ -5,6 +5,9 @@ namespace Settings
     public class SettingsClass
     {
         public string SecretPhrase { get; set; }
+
         public string PublicValue { get; set; }
+
+        public int PublicInt { get; set; }
     }
 }
